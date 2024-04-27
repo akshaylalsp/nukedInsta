a0ab668586965316112bcbc3c2baf8573186d098
akshaylalsp <akshaylalsp@gmail.com>
2024-04-27 15:13:04 +0530

    ui

---
+++ app/src/main/java/com/example/webview/NavigationComponet.kt
@@ -0,0 +1,35 @@
+package com.example.webview
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NavBar(){
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // WebView Here
+        Row(
+            modifier = Modifier.fillMaxWidth().
+        ){
+            Button(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "")
+            }
+        }
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Button(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "")
+        }
+    }
+}
