d1cca38186530c72758c6530dc334fcce5192262
akshaylalsp <akshaylalsp@gmail.com>
2024-04-27 22:34:02 +0530

    removed unwanted codes

--- app/src/main/java/com/example/webview/WebView_Screen.kt
+++ app/src/main/java/com/example/webview/WebView_Screen.kt
@@ -18,9 +18,6 @@ import androidx.compose.ui.viewinterop.AndroidView
 @Composable
 fun WebView(mUrl:String) {
 
-    // ... (rest of the code)
-//    val mUrl = "https://www.instagram.com/direct/inbox"
-
     AndroidView(factory = {
         val webView = WebView(it).apply {
             layoutParams = ViewGroup.LayoutParams(
@@ -42,15 +39,6 @@ fun WebView(mUrl:String) {
                             }, 0); // Delay by 3 second
                         """.trimIndent()
 
-
-//                @Deprecated("Deprecated in Java", ReplaceWith("false"))
-//                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-//                    // Handle link clicks here (optional)
-//                    // ...
-//                    view.evaluateJavascript(jsCode, null)
-//                    return false
-//                }
-
                 override fun onLoadResource(view: WebView?, url: String?) {
                     super.onLoadResource(view, url)
                     view?.evaluateJavascript(jsCode, null)
@@ -60,31 +48,15 @@ fun WebView(mUrl:String) {
                     view: WebView?,
                     request: WebResourceRequest?
                 ): Boolean {
-                    // Re-inject JavaScript if the URL changes
                     view?.evaluateJavascript(jsCode, null)
                     return super.shouldOverrideUrlLoading(view, request)  // Let WebView handle the URL change
                 }
 
-
-                //                override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
-//                    super.onPageStarted(view, url, favicon)
-//                    view.evaluateJavascript(jsCode, null)
-//                    // Show a loading indicator while page loads
-//                    // (implement your loading indicator UI here)
-//                }
-//
                 override fun onPageFinished(view: WebView, url: String) {
                     super.onPageFinished(view, url)
                     view.evaluateJavascript(jsCode, null)
-                    // Hide loading indicator after page finishes loading
                 }
-//
-//                override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
-//                    super.onReceivedError(view, request, error)
-//                    view.reload()
-//                    // Handle loading errors (display an error message or retry)
-//                    // (implement your error handling logic here)
-//                }
+
             }
         }
         webView // Return the created WebView instance
